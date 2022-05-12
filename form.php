90ce40ee46f50a651e07f2180f94bafcf2784d4c
cristian almaraz <cristian28_lp@hotmail.com>
2022-05-12 19:06:32 -0300

    falta el paso de script para terminar el formulario

---
+++ form.php
@@ -0,0 +1,21 @@
+<?php
+//captutar la información
+$nombre = $_POST ["nombre"];
+
+$mail = $_POST ["mail"];
+
+$mensaje = $_POST ["textarea"];
+
+
+//a quien le llegara
+$para = "cristian28_lp@hotmail.com";
+$asunto = "este mail fue enviado desde mi web";
+
+//funcionalidad
+
+mail($para,$asunto, utf8_decode($mensaje));
+
+//redirigir al usuario una vez completado el form
+header("Location:exito.html")
+
+?>
